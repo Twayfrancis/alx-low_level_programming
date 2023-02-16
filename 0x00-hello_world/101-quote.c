b5c63d9b53ac4ca4c69a80a8f31551c57ca4927e
Francis Tawai <122815843+Twayfrancis@users.noreply.github.com>
2023-02-16 20:35:07 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main -understand the simplicity
-*Return: 1 (success)
+* main -understand the simplicity
+* Return: 1 (success)
 */
 int main(void)
 {
-    write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+    write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
     return (1); 
 }
