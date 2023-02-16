b3cde56e92f6c0fffd132b3427d67629031af6d7
Francis Tawai <122815843+Twayfrancis@users.noreply.github.com>
2023-02-16 20:32:39 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
 *main -understand the simplicity
-*Return: Always 1 (success)
+*Return: 1 (success)
 */
 int main(void)
 {
-    fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-    return (1);
+    write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+    return (1); 
 }
