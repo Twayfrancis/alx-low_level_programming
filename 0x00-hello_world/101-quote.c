e472387b99679a990341ea569b2e789dc86a3b4d
Francis Tawai <122815843+Twayfrancis@users.noreply.github.com>
2023-02-16 15:52:31 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -genius to understand the simplicity
+ *main -genius to understand the simplicity
  *
- * Return: Always 1 (success)
+ *Return: Always 1 (success)
  */
 int main(void)
 {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
     return 1;
 }
