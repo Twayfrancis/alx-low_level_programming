6ab5708a949d78c9d0a70cc7ada54a6b71a9efa8
Twayfrancis <tawaifrancis@gmail.com>
2023-02-16 02:25:51 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program printing line using puts
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
