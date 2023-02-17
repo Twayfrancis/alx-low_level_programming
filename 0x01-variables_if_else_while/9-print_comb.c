4ac672c5914079871a22eea92c22af6bdf938fdb
Twayfrancis <tawaifrancis@gmail.com>
2023-02-17 11:01:30 -0800

    9. Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+for (j = i; j < 10; j++)
+putchar(i * 10 + j + '0');
+putchar(j < 9 ? ',' : '\n');
+return (0);
+}
