6cd24c304e65a937dbcb3d8e4c91c1eab1c3a5da
Francis Tawai <122815843+Twayfrancis@users.noreply.github.com>
2023-02-18 00:11:16 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-int n, m, l;
+int n, m, |;
 for (n = 48; n < 58; m++)
 {
 for (m = 49; m < 58; m++)
 {
-for (m > l && m > n)
+for (| > m && m > n)
 {
 putchar(n);
 putchar(m);
-putchar(l);
+putchar(|);
 if (n != 55 || m != 56)
 {
 putchar(',');
