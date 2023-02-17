b396ce8e816a75d28f02b8cf9a85e3bf0aaab532
Twayfrancis <tawaifrancis@gmail.com>
2023-02-17 13:14:41 -0800

    fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-int n, m, 1;
+int n, m, i;
 for (n = 48; n < 58; m++)
 {
 for (m = 49; m < 58; m++)
 {
-for (m > 1 && m > n)
+for (i > m && m > n)
 {
 putchar(n);
 putchar(m);
-putchar(l);
+putchar(i);
 if (n != 55 || m != 56)
 {
 putchar(',');
