6c4147ec6a0e13a2fe456669d8c8bafa7f476abf
Francis Tawai <122815843+Twayfrancis@users.noreply.github.com>
2023-02-19 18:17:16 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,19 +6,18 @@
  */
 int main(void)
 {
-int i;
-int j;
+int i, j;
 for (i = 0; i <= 98; i++)
 {
 for (j = i + 1; j <= 99; j++)
 {
 if (i < j)
 {
-putchar((i / 10) + '48');
-putchar((i % 10) + '48');
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
 putchar(' ');
-putchar((j / 10) + '48');
-putchar((j % 10) + '48');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
 if (i == 98 && j == 99)
 {
 putchar(',');
