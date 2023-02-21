6d654f36b80ccef4e39967f494c39bae34f0080a
Twayfrancis <tawaifrancis@gmail.com>
2023-02-21 10:13:12 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *  print_alphabet_x10 -  prints 10 times the alphabet, in lowercase
+ *  author Tway
+ *  Return: x10 a-z
+ */
+void print_alphabet_x10(void);
+{
+	int n, dw;
+
+	dw = 0;
+
+	while (dw < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		dw++;
+		_putchar('\n');
+	}
+}
