30cefb8eb5eba0b5c4dd49ad0a318c88d91ee911
Twayfrancis <tawaifrancis@gmail.com>
2023-02-22 02:15:01 -0800

    fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,15 @@ int main(void)
 {
 int n;
 int sum;
+
 sum = 0;
 for (n = 0; n < 1024; n++)
 {
 if ((n % 3 == 0 || (n % 5 == 0)))
 {
-sum = sum + n;
+sum = sum += n;
 }
 printf("%d\n", sum);
+return (0);
 }
 }
