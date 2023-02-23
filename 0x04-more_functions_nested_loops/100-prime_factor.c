1744b896cd9e60253abc2f3c947009daf699bd5a
Twayfrancis <tawaifrancis@gmail.com>
2023-02-23 13:25:22 -0800

    The prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - The prime factors of 1231952 are 2, 2, 2, 2, 37 2081
+ * Author Tway
+ * using standard library
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	return (0);
+}
