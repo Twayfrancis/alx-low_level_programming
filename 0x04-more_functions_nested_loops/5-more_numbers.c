f5e41d430c0cab8c83621b6e4da978146c7eb5de
Francis Tawai <122815843+Twayfrancis@users.noreply.github.com>
2023-02-23 22:06:34 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,16 @@
 void more_numbers(void)
 {
 	int i, j;
-	for (i = 0; i < 10; i++)
+
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 0; j < 15; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (j >= 10)
+			if (i >= 10)
 			{
-				_putchar(j / 10 + '0');
+				_putchar((i / 10) + '0');
 			}
-			_putchar(j % 10 + '0');
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
