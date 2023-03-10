8b89c0d27b3958325cda36dc7b8776fc5a709bb9
Twayfrancis <tawaifrancis@gmail.com>
2023-03-10 11:39:59 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main -  prints its name, followed by a new line.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 always (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
