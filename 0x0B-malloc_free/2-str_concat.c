b3ca8a0d4ae1653a6cced869170afb5599b7eb72
Twayfrancis <tawaifrancis@gmail.com>
2023-03-13 10:48:09 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,13 +7,15 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int len1 = 0, len2 = 0;
+	int len1, len2;
 	char *concat;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	len1 = len2 = 0;
 
 	while (s1[len1] != '\0')
 		len1++;
@@ -25,6 +27,8 @@ char *str_concat(char *s1, char *s2)
 
 	if (concat == NULL)
 		return (NULL);
+
+	len1 = len2 = 0;
 
 	while (s1[len1] != '\0')
 	{
