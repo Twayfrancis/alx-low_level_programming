124b9b73202dccb7cc27dbe2f8d8bd7b25a678ab
Twayfrancis <tawaifrancis@gmail.com>
2023-03-16 12:45:19 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <limits.h>
 
-int _putchar(char *);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
