ddbf32a791d43a29152a3cbcd3d607b8427f3f29
Twayfrancis <tawaifrancis@gmail.com>
2023-03-20 10:07:30 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints name of file it was compiled from, followed by new line
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("Nmae of source file: %s\n", __FILE__);
+	return (0);
+}
