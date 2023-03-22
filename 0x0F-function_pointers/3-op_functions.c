15c8a5ac061d7dff84e4d9220309d61d46e8c83f
Twayfrancis <tawaifrancis@gmail.com>
2023-03-22 12:06:29 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - returns sum of a amd b
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns difference of a and b
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns product of a and b
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns resut of division of a by b
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns remainder of division of a by b
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
