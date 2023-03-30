63437a6810e297ee30e2743949cdd54f0177288b
Twayfrancis <tawaifrancis@gmail.com>
2023-03-30 12:27:08 -0700

    2. Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,14 +9,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+
 	if (str == NULL)
 		return (NULL);
-	
+
 	new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
+
 	if (new_node->str == NULL)
 	{
 		free(new_node);
