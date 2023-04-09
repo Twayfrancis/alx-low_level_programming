d8b7c03b0a6fe134149ea781e8810edd4bfed26d
Francis Tawai <tawaifrancis@gmail.com>
2023-04-09 09:21:39 +0300

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * append_text_to_file- appends text at the end of a file
+ * @filename: is the name of the file
+ * @text_content: is the NULL terminated string to add at the end of the file
+ * Author Tway
+ * Return: 1 on success and -1 on failure
+ * If filename is NULL return -1
+ * If text_content is NULL, do not add anything to the file
+ * Return 1 if the file exists and -1 if the file does not exist 
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+    if (filename == NULL)
+    {
+        return (-1);
+    }
+    FILE *file = fopen(filename, 'a');
+    {
+        return (-1);
+    }
+
+    if (text_content != NULL)
+    {
+        fputs(text_content, file);
+    }
+    fclose(file);
+    return (-1);
+}
