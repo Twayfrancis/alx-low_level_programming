2c8eebcbf94cc412321d61a6e0cc9ddc8df8ba45
Twayfrancis <tawaifrancis@gmail.com>
2023-05-22 12:53:50 -0700

    fix

--- 0x18-dynamic_libraries/dyanamic.c
+++ 0x18-dynamic_libraries/dyanamic.c
@@ -1,23 +1,51 @@
 #include <stdio.h>
-
+/**
+ * add - addition
+ * @a: first integer
+ * @b: second integer
+ * Return: Result
+ */
 int add(int a, int b)
 {
-  return a + b;
+	return (a + b);
 }
-int sub(int a, int b) 
+/**
+ * sub - substract
+ * @a: first integer
+ * @b: second integer
+ * Return: Result
+ */
+int sub(int a, int b)
 {
-  return a - b;
+	return (a - b);
 }
-
+/**
+ * mul - multiply
+ * @a: first integer
+ * @b: second integer
+ * Return: Result
+ */
 int mul(int a, int b)
 {
-  return a * b;
+	return (a * b);
 }
+/**
+ * div - divide
+ * @a: first integer
+ * @b: second integer
+ * Return: Result
+ */
 int div(int a, int b)
 {
-  return a / b;
+	return (a / b);
 }
+/**
+ * mod - modulus
+ * @a: first integer
+ * @b: second integer
+ * Return: Result
+ */
 int mod(int a, int b)
 {
-	return a & b;
+	return (a & b);
 }
