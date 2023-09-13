6055569c5f8ccf11597d97e270d9d965c4e98f44
Twayfrancis <tawaifrancis@gmail.com>
2023-09-13 10:22:05 -0700

    fix betty style

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,5 +1,11 @@
 #include "search_algos.h"
 /**
+ * interpolation_search - search for value in sorted array
+ * @array: pointer to first element of array to search
+ * @size: number of elements in array
+ * @value: the value to searc for
+ * Return: first index where value is located
+ * -1 if not found or error
  */
 int interpolation_search(int *array, size_t size, int value)
 {
@@ -13,24 +19,19 @@ int interpolation_search(int *array, size_t size, int value)
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
 		size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
 		if (array[pos] == value)
-		{
 			return (pos);
-		}
 
 		if (array[pos] < value)
-		{
 			low = pos + 1;
-		}
 		else
-		{
 			high = pos - 1;
-		}
 	}
 	if (value < array[low] || value > array[high])
-		printf("Value checked array[%lu] is out of range\n", low);
+		printf("Value checked array[%lu] is out of range\n", high);
 
 	return (-1);
 }
